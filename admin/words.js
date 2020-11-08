33ac1b6cc940a585d41323462079177cbcdb0785
Nils Seinschedt <n.seinschedt@neusta.de>
2020-11-08 11:58:53 +0100

    initial

---
+++ admin/words.js
@@ -0,0 +1,101 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    "App ID": {
+        "en": "App ID",
+        "de": "App ID",
+        "ru": "ID приложения",
+        "pt": "ID do aplicativo",
+        "nl": "App-ID",
+        "fr": "ID d'application",
+        "it": "ID app",
+        "es": "ID de aplicación",
+        "pl": "Identyfikator aplikacji",
+        "zh-cn": "应用程式编号"
+    },
+    "App Password": {
+        "en": "App Password",
+        "de": "App Passwort",
+        "ru": "Пароль приложения",
+        "pt": "Senha do app",
+        "nl": "App-wachtwoord",
+        "fr": "Mot de passe de l'application",
+        "it": "Password dell'app",
+        "es": "Contraseña de la aplicación",
+        "pl": "Hasło aplikacji",
+        "zh-cn": "应用密码"
+    },
+    "App Tenant": {
+        "en": "App Tenant",
+        "de": "App Tenant",
+        "ru": "Клиент приложения",
+        "pt": "App Tenant",
+        "nl": "App Huurder",
+        "fr": "Locataire de l'application",
+        "it": "Tenant dell'app",
+        "es": "Inquilino de la aplicación",
+        "pl": "Najemca aplikacji",
+        "zh-cn": "应用租户"
+    },
+    "Proxy port": {
+        "en": "Proxy port",
+        "de": "Proxy-Port",
+        "ru": "Порт прокси",
+        "pt": "Porta proxy",
+        "nl": "Proxy-poort",
+        "fr": "Port proxy",
+        "it": "Porta proxy",
+        "es": "Puerto proxy",
+        "pl": "Port proxy",
+        "zh-cn": "代理端口"
+    },
+    "Update interval in seconds": {
+        "en": "Update interval in seconds",
+        "de": "Aktualisierungsintervall in Sekunden",
+        "ru": "Интервал обновления в секундах",
+        "pt": "Intervalo de atualização em segundos",
+        "nl": "Update-interval in seconden",
+        "fr": "Intervalle de mise à jour en secondes",
+        "it": "Aggiorna l'intervallo in secondi",
+        "es": "Intervalo de actualización en segundos",
+        "pl": "Interwał aktualizacji w sekundach",
+        "zh-cn": "更新间隔（以秒为单位）"
+    },
+    "Authenticate with Microsoft": {
+        "en": "Authenticate with Microsoft",
+        "de": "Authentifizieren Sie sich bei Microsoft",
+        "ru": "Аутентифицироваться в Microsoft",
+        "pt": "Autenticar com a Microsoft",
+        "nl": "Verifieer bij Microsoft",
+        "fr": "S'authentifier auprès de Microsoft",
+        "it": "Autentica con Microsoft",
+        "es": "Autenticarse con Microsoft",
+        "pl": "Uwierzytelnij się w firmie Microsoft",
+        "zh-cn": "与Microsoft进行身份验证"
+    },
+    "Please save before reauthenticate with Microsoft": {
+        "en": "Please save before reauthenticate with Microsoft",
+        "de": "Bitte speichern Sie, bevor Sie sich erneut bei Microsoft authentifizieren",
+        "ru": "Пожалуйста, сохраните перед повторной аутентификацией в Microsoft",
+        "pt": "Salve antes de se autenticar novamente com a Microsoft",
+        "nl": "Sla op voordat u opnieuw verifieert bij Microsoft",
+        "fr": "Veuillez enregistrer avant de vous authentifier à nouveau avec Microsoft",
+        "it": "Salvare prima di eseguire nuovamente l'autenticazione con Microsoft",
+        "es": "Guarde antes de volver a autenticarse con Microsoft",
+        "pl": "Zapisz przed ponownym uwierzytelnieniem w firmie Microsoft",
+        "zh-cn": "请保存，然后再与Microsoft重新认证"
+    },
+    "Authenticated as:": {
+        "en": "Authenticated as:",
+        "de": "Authentifiziert als:",
+        "ru": "Аутентифицирован как:",
+        "pt": "Autenticado como:",
+        "nl": "Geverifieerd als:",
+        "fr": "Authentifié comme:",
+        "it": "Autenticato come:",
+        "es": "Autenticado como:",
+        "pl": "Uwierzytelniony jako:",
+        "zh-cn": "认证为："
+    }
+};
---
+++ main.js
@@ -0,0 +1,244 @@
+'use strict';
+
+const utils = require('@iobroker/adapter-core');
+const http = require('http');
+const ipInfo = require('ip');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+const OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
+const graph = require('@microsoft/microsoft-graph-client');
+require('isomorphic-fetch');
+
+const oauthAuthorityHost = 'https://login.microsoftonline.com/';
+const oauthAuthorizeEndpoint = 'oauth2/v2.0/authorize';
+const oauthTokenEndpoint = 'oauth2/v2.0/token';
+const oauthIdMetadata = 'v2.0/.well-known/openid-configuration';
+const oauthScopes = ['profile','user.read','offline_access','Presence.Read'];
+let updateIntervalId = null;
+
+class MsTeams extends utils.Adapter {
+    constructor(options) {
+        super({
+            ...options,
+            name: 'ms-teams',
+        });
+        this.on('ready', () => {
+            this.getForeignObject('system.adapter.admin.0',  (err, obj) => {
+                this.onReady.bind(this)(obj.native.port || 8081);
+            });
+        });
+        this.on('unload', this.onUnload.bind(this));
+    }
+
+    async onReady(adminPort) {
+        const adapter = this;
+        let proxyConnected = false;
+        let currentUser = null;
+        const users = {};
+        const oauthAuthorityUrl = oauthAuthorityHost + (this.config.appTenant || 'common') + '/';
+        let port = parseInt(this.config.proxyPort, 10);
+        port = isNaN(port) ? this.config.proxyPort : (port >= 0 ? port : false);
+        const adminConfigUrl = 'http://' + ipInfo.address() + ':' + adminPort + '/#tab-instances/config/system.adapter.ms-teams.' + adapter.instance;
+
+        await this.setObjectNotExistsAsync('availability', {
+            type: 'state',
+            common: {
+                name: 'availability',
+                type: 'state',
+                role: 'state',
+                read: true,
+                write: true,
+            },
+            native: {},
+        });
+        await this.setObjectNotExistsAsync('activity', {
+            type: 'state',
+            common: {
+                name: 'activity',
+                type: 'state',
+                role: 'state',
+                read: true,
+                write: true,
+            },
+            native: {},
+        });
+
+        const getAccessToken = async (req) => {
+            if (!req.isAuthenticated()) {
+                throw(new Error('Not authenticated'));
+            }
+            if (!req.user) {
+                throw(new Error('No user in request'));
+            }
+            if (!req.user.oauthToken) {
+                throw(new Error('No stored token'));
+            }
+            if (req.user.oauthToken.expired()) {
+                req.user.oauthToken = await req.user.oauthToken.refresh();
+            }
+            return req.user.oauthToken.token.access_token;
+        };
+        function getAuthenticatedClient(accessToken) {
+            return graph.Client.init({
+                authProvider: (done) => {
+                    done(null, accessToken);
+                }
+            });
+        }
+
+        const oauth2 = require('simple-oauth2').create({
+            client: {
+                id: this.config.appId,
+                secret: this.config.appPassword
+            },
+            auth: {
+                tokenHost: oauthAuthorityUrl,
+                authorizePath: oauthAuthorizeEndpoint,
+                tokenPath: oauthTokenEndpoint
+            }
+        });
+        passport.serializeUser(function(user, done) {
+            users[user.profile.oid] = user;
+            done (null, user.profile.oid);
+        });
+        passport.deserializeUser(function(id, done) {
+            done(null, users[id]);
+        });
+        passport.use(new OIDCStrategy(
+            {
+                identityMetadata: oauthAuthorityUrl + oauthIdMetadata,
+                clientID: this.config.appId,
+                responseType: 'code id_token',
+                responseMode: 'form_post',
+                redirectUrl: 'http://' + ipInfo.address() + ':' + port +'/auth/callback',
+                allowHttpForRedirectUrl: true,
+                clientSecret: this.config.appPassword,
+                validateIssuer: false,
+                passReqToCallback: false,
+                scope: oauthScopes
+            },
+            async (iss, sub, profile, accessToken, refreshToken, params, done) => {
+                if (!profile.oid) {
+                    return done(new Error('No OID found in user profile.'), null);
+                }
+                const oauthToken = oauth2.accessToken.create(params);
+                users[profile.oid] = { profile, oauthToken };
+                return done(null, users[profile.oid]);
+            }
+        ));
+
+        const app = express();
+        app.set('port', port);
+        app.use(session({
+            secret: 'dsakbb12-adsj78lkn-klmsda',
+            resave: false,
+            saveUninitialized: false,
+            unset: 'destroy'
+        }));
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: false }));
+        app.use(cookieParser());
+        app.use(passport.initialize());
+        app.use(passport.session());
+        app.use(async function(req, res, next) {
+            if (req.session && req.user) {
+                const update = async () => {
+                    try {
+                        const client = getAuthenticatedClient(await getAccessToken(req));
+                        const presence = await client.api('/me/presence').version('beta').get();
+                        if (req && req.session) {
+                            req.session.presence = presence;
+                        }
+                        adapter.setState('availability', presence.availability);
+                        adapter.setState('activity', presence.activity);
+                        adapter.setState('info.connection', true, true);
+                        currentUser = req.user;
+                    } catch (err) {
+                        adapter.setState('info.connection', false, true);
+                        currentUser = null;
+                        adapter.log.error('Failed to get presence: ' + err);
+                    }
+                };
+                updateIntervalId = setInterval(update, (adapter.config.updateInterval || 10) * 1000);
+                update();
+            }
+            next();
+        });
+
+        app.use('/auth/signin', function  (req, res, next) {
+            passport.authenticate('azuread-openidconnect',
+                {
+                    response: res,
+                    prompt: 'login',
+                    failureRedirect: '/',
+                    successRedirect: '/'
+                }
+            )(req,res,next);
+        });
+        app.use('/auth/callback', function(req, res, next) {
+            passport.authenticate('azuread-openidconnect',
+                {
+                    response: res,
+                    failureRedirect: '/',
+                    successRedirect: '/'
+                }
+            )(req,res,next);
+        });
+        app.use('/', function  (req, res) {
+            res.redirect(adminConfigUrl);
+        });
+
+        const server = http.createServer(app);
+        server.listen(port);
+        server.on('error', (error) => {
+            if (error.syscall !== 'listen') {
+                throw error;
+            }
+            const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+            switch (error.code) {
+                case 'EACCES':
+                    this.log.error(bind + ' requires elevated privileges');
+                    process.exit(1);
+                    break;
+                case 'EADDRINUSE':
+                    this.log.error(bind + ' is already in use');
+                    process.exit(1);
+                    break;
+                default:
+                    throw error;
+            }
+        });
+        server.on('listening', () => {
+            const addr = server.address();
+            this.log.info('Listening on ' + (typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port));
+            proxyConnected = true;
+        });
+
+        adapter.on('message', function(msg) {
+            switch (msg.command) {
+                case 'getStatusInfo':
+                    adapter.sendTo(msg.from, msg.command, { proxyConnected, currentUser }, msg.callback);
+                    break;
+            }
+        });
+        adapter.setState('info.connection', false, true);
+    }
+
+    onUnload(callback) {
+        try {
+            clearInterval(updateIntervalId);
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+}
+
+// @ts-ignore parent is a valid property on module
+if (module.parent) {
+    module.exports = (options) => new MsTeams(options);
+} else {
+    new MsTeams();
+}
